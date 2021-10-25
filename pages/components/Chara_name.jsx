b46ff9480c078bbd97aced04ea5c11b4224280fe
OkamiMitsuki <OkamiMitsuki247@gmail.com>
2021-10-25 21:10:28 +0900

    SSGtest

---
+++ pages/components/Chara_name.jsx
@@ -0,0 +1,12 @@
+const Name = () => {
+  return (
+    <div>
+      <ul>
+        <li> <h2>Yukihana</h2> <h2>Lamy</h2> </li>
+        <li> <h1>雪花</h1> <h1>ラミィ</h1> </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Name
