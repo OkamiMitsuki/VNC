d61b2107117175361f7109a3eef5e4b991e49f8a
OkamiMitsuki <OkamiMitsuki247@gmail.com>
2021-10-28 05:22:05 +0900

    material_ui追加

---
+++ datas/names.js
@@ -0,0 +1,28 @@
+export const names = {
+  "lamy": {
+    "nicname": "Lamy",
+    "first_name_en": "Yukihana",
+    "first_name_jp": "雪花",
+    "last_name_en": "Lamy",
+    "last_name_jp": "ラミィ",
+    "mark": "⛄",
+    "youtube": "https://www.youtube.com/channel/UCFKOVgVbGmX65RxO3EtH3iw?sub_confirmation=1",
+    "twitter": "https://twitter.com/yukihanalamy",
+    "homepage": "https://hololive.hololivepro.com/talents/yukihana-lamy/",
+    "wiki": "https://seesaawiki.jp/hololivetv/d/%C0%E3%B2%D6%A5%E9%A5%DF%A5%A3",
+    "fun_disco": ""
+  },
+  "marin": {
+    "nicname": "marin",
+    "first_name_en": "Houshou",
+    "first_name_jp": "宝鐘",
+    "last_name_en": "Marine",
+    "last_name_jp": "マリン",
+    "mark": "🏴‍☠️",
+    "youtube": "https://www.youtube.com/channel/UCCzUftO8KOVkV4wQG1vkUvg?sub_confirmation=1",
+    "twitter": "https://twitter.com/houshoumarine",
+    "homepage": "https://hololive.hololivepro.com/talents/houshou-marine/",
+    "wiki": "https://seesaawiki.jp/hololivetv/d/%CA%F5%BE%E2%A5%DE%A5%EA%A5%F3",
+    "fun_disco": ""
+  }
+}
--- pages/components/Chara_name.jsx
+++ pages/components/Chara_name.jsx
@@ -1,10 +1,14 @@
+import { names } from "../../datas/names"
+const name_set = names.lamy
+console.log(name_set)
 const Name = () => {
   return (
     <div>
       <ul>
-        <li> <h2>Yukihana</h2> <h2>Lamy</h2> </li>
-        <li> <h1>雪花</h1> <h1>ラミィ</h1> </li>
+        <li> <h2>{name_set.first_name_en}</h2> <h2>{name_set.last_name_en}</h2> </li>
+        <li> <h1>{name_set.first_name_jp}</h1> <h1>{name_set.last_name_jp}</h1> </li>
       </ul>
+      <h3>{name_set.mark}</h3>
     </div>
   );
 }
