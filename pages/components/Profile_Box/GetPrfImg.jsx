b8acdad09b3605dcbe71d23bf5122fa5146232ca
OkamiMitsuki <OkamiMitsuki247@gmail.com>
2021-11-14 03:56:33 +0900

    プロフィール動画埋め込み

--- pages/components/Profile_Box/GetPrfImg.jsx
+++ pages/components/Profile_Box/GetPrfImg.jsx
@@ -1,8 +1,10 @@
 import Styles from '../../../public/css/style.module.css'
 
 export default function GetPrfImg() {
-  return <img className={Styles.imgs}
-    src={`/Lamy/LamyPrf2.png`}
-    alt={`ラミィの画像`}
-    title={`プロフィール画像`} />;
+  return <video
+    muted
+    autoPlay
+    playsInline
+    loop
+    src="/Lamy/idol.mp4" />;
 }
